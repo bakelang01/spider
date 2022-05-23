159ee087f751084a5033030131b691a61c4a90f8
BlackLang <97121188+bakelang01@users.noreply.github.com>
2022-05-23 18:29:08 +0800

    Update get_url.py

--- 天眼查获取公司信息/get_url.py
+++ 天眼查获取公司信息/get_url.py
@@ -33,6 +33,12 @@ def res(key):
     return href
 
 
+"""
+传入一份全是公司全名称的表格，然后先会爬取每一个公司的详情页的url 并保存在一个csv文件中，再有由另一个程序代码进行信息的获取存储
+之所以没有合并两个py文件，是因为懒
+如果想要公司其他信息，也可以解析其他对应信息
+"""
+
 filepath = 'commpanys.xlsx'
 com_li = get_excel(filepath)
 with open('hrefs_1.csv','w',encoding='utf-8') as f:
